99c4db73134d37c61d7e8d5b007b5a2ed3c701f2
mikaely <mikael.ylivaara@metropolia.fi>
2020-04-08 09:33:06 +0300

    a

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict';
 const express = require('express');
 const session = require('express-session');
+const passport = require('./utils/pass');
 const app = express();
 const port = 3000;
+
+const loggedIn = (req, res, next) => {
+  console.log('logged', req.user);
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
@@ -15,6 +25,9 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -36,22 +49,20 @@ app.get('/form', (req, res) => {
   res.render('form');
 });
 
-app.get('/secret', (req, res) => {
-  if (req.session.logged) {
-    res.render('secret');
-  } else {
-    res.redirect('/form');
-  }
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
 
-app.post('/login', (req, res) => {
-  if (username === req.body.username && password === req.body.password) {
-    req.session.logged = true;
-    res.redirect('/secret');
-  } else {
-    req.session.logged = false;
-    res.redirect('/form');
-  }
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/form'}),
+    (req, res) => {
+      console.log('success');
+      res.redirect('/secret');
+    });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/form');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
